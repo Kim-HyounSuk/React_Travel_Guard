0d718b6cc9c8c2e3320d63954c610ed2c451cb77
Kim-HyounSuk <kkoor586@naver.com>
2024-09-30 02:14:31 +0900

    Feat: useTabs 커스텀 훅 수정(return 값 수정)

--- src/hooks/useTabs.ts
+++ src/hooks/useTabs.ts
@@ -1,7 +1,7 @@
 import ICombinedData from '@/types/combinedData';
 import { useMemo, useState } from 'react';
 
-interface ITab {
+export interface ITab {
 	label: string;
 	value: number | null;
 }
@@ -13,7 +13,6 @@ const tabs: ITab[] = [
 	{ label: '2단계 여행자제', value: 2 },
 	{ label: '3단계 출국권고', value: 3 },
 	{ label: '4단계 여행금지', value: 4 },
-	{ label: '5단계 특별여행주의보', value: 5 },
 ];
 
 const useTabs = (data: ICombinedData[]) => {
