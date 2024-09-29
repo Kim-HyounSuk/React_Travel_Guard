5816b6ac8c34b05e463a16655fa3033ab40615a3
Kim-HyounSuk <kkoor586@naver.com>
2024-09-29 23:55:04 +0900

    Feat: BadgeText 컴포넌트 export 추가

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@ export { default as Badge } from './Badge';
 export { default as Box } from './Box';
 export { default as Title } from './Title';
 export { default as SearchInput } from './SearchInput';
+export { default as BadgeText } from './BadgeText';
