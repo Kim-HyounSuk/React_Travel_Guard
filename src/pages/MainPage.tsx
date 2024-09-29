4f7ff36b778f5667b6740dbc3464aac80834e7f4
Kim-HyounSuk <kkoor586@naver.com>
2024-09-29 23:01:08 +0900

    Feat: Main 페이지 작성(검색 로직 구현)

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,43 @@
-import { SearchInput } from '@/components/common';
+import { SearchInput, Title } from '@/components/common';
+import ICombinedData from '@/types/combinedData';
+import useCombinedData from '@/utils/useCombinedData';
+import { useEffect, useState } from 'react';
 
 const MainPage = () => {
-	// Search 로직 구현하면 됨.
-	const onSearch = (query: string) => {};
+	// 복합 데이터 fetch
+	const { data } = useCombinedData();
+	const [countries, setCountries] = useState<ICombinedData[]>([]);
+
+	useEffect(() => {
+		if (!data) return;
+
+		setCountries(data);
+	}, [data]);
+
+	const onSearch = (query: string) => {
+		if (!data) return;
+
+		const lowerQuery = query.toLowerCase();
+
+		const filteredData = data?.filter(
+			(country) =>
+				country.country_nm.toLowerCase().includes(lowerQuery) ||
+				country.country_eng_nm.toLowerCase().includes(lowerQuery),
+		);
+
+		setCountries(filteredData);
+	};
 
 	return (
-		<div>
+		<>
+			<Title
+				data={{
+					title: '국가별 정보',
+					text: '국가별 현지 연락처, 사건•사고 정보, 문화 등 다양한 정보를 제공합니다.',
+				}}
+			/>
 			<SearchInput onSearch={onSearch} placeholder={'국가명을 입력하세요.'} />
-		</div>
+		</>
 	);
 };
 
