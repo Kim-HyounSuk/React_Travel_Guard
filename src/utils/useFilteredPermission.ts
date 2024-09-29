d2963cc2e2c950345c1933d9073e21ffb2e78927
Kim-HyounSuk <kkoor586@naver.com>
2024-09-30 05:23:55 +0900

    Refactor: useFilteredPermission 함수 수정(주석 추가)

--- src/utils/useFilteredPermission.ts
+++ src/utils/useFilteredPermission.ts
@@ -1,6 +1,7 @@
 import ICombinedData from '@/types/combinedData';
 import { useMemo } from 'react';
 
+/** combinedData 중 entry 프로퍼티가 있는 정보만 필터링 하는 함수 */
 const useFilteredPermission = (data: ICombinedData[] | null) => {
 	return useMemo(() => {
 		if (!data) return null;
