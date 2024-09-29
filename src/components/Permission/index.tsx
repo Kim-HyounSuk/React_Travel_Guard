ba60a49b66f5a800fa9844bb39be792d08d06292
Kim-HyounSuk <kkoor586@naver.com>
2024-09-30 06:10:29 +0900

    Feat: PermissionPage 컴포넌트 작성

---
+++ src/components/Permission/index.tsx
@@ -0,0 +1,129 @@
+import ICombinedData from '@/types/combinedData';
+import styled from '@emotion/styled';
+import React from 'react';
+import { BadgeText, Box } from '@/components/common';
+
+interface IProps {
+	data: ICombinedData[];
+}
+
+const Permission: React.FC<IProps> = ({ data }) => {
+	return (
+		<Container>
+			<Title>
+				<span>국가</span>
+				<span>입국 기간</span>
+				<span>입국 여부</span>
+				<span>여권 소지</span>
+				<span>무비자 근거</span>
+			</Title>
+			<Contents>
+				{data.length > 0 ? (
+					data.map((country) => (
+						<Content>
+							<BadgeText isTab={true} color={country.color}>
+								{country.country_nm}
+							</BadgeText>
+							<span>{country.entry?.entry_period}</span>
+							<span>{country.entry?.entry_status}</span>
+							<span>{country.entry?.entry_possession}</span>
+							<span>{country.entry?.entry_ground}</span>
+						</Content>
+					))
+				) : (
+					<NonContent>조건에 맞는 데이터가 없습니다.</NonContent>
+				)}
+			</Contents>
+		</Container>
+	);
+};
+
+export default Permission;
+
+const Container = styled(Box)`
+	display: flex;
+	flex-direction: column;
+	flex: 1;
+	width: 100%;
+	min-height: 0;
+	margin: 3rem 0;
+
+	@media (max-width: ${({ theme }) => theme.layout.maxWidth.mobile}) {
+		padding: 0.5rem;
+	}
+`;
+const Title = styled.div`
+	width: 100%;
+	display: flex;
+	align-items: center;
+	font-size: ${({ theme }) => theme.fontSizes.medium};
+	padding: 0 0 1rem 0;
+	border-bottom: 1px solid ${({ theme }) => theme.colors.primary};
+
+	& > span {
+		flex: 1;
+		text-align: center;
+		color: ${({ theme }) => theme.colors.primary};
+	}
+
+	& > span:nth-of-type(1) {
+		min-width: 125px;
+	}
+
+	@media (max-width: ${({ theme }) => theme.layout.maxWidth.tablet}) {
+		font-size: ${({ theme }) => theme.fontSizes.regular};
+		& > span:nth-last-of-type(1) {
+			display: none;
+		}
+	}
+
+	@media (max-width: ${({ theme }) => theme.layout.maxWidth.mobile}) {
+		padding: 0 0 0.5rem 0;
+		font-size: ${({ theme }) => theme.fontSizes.small};
+	}
+`;
+const Contents = styled.div`
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	padding-top: 1rem;
+	width: 100%;
+	gap: 1rem;
+	overflow: auto;
+
+	&::-webkit-scrollbar {
+		width: 4px;
+	}
+	&::-webkit-scrollbar-thumb {
+		border-radius: 2px;
+		background: #f0f0f0;
+	}
+`;
+const Content = styled.div`
+	display: flex;
+	align-items: center;
+
+	& > * {
+		flex: 1;
+		text-align: center;
+	}
+
+	@media (max-width: ${({ theme }) => theme.layout.maxWidth.tablet}) {
+		font-size: ${({ theme }) => theme.fontSizes.small};
+
+		& > span:nth-last-of-type(1) {
+			display: none;
+		}
+	}
+`;
+const NonContent = styled.div`
+	display: flex;
+	width: 100%;
+	height: 100%;
+	align-items: center;
+	justify-content: center;
+
+	@media (max-width: ${({ theme }) => theme.layout.maxWidth.mobile}) {
+		font-size: ${({ theme }) => theme.fontSizes.small};
+	}
+`;
--- src/pages/PermissionPage.tsx
+++ src/pages/PermissionPage.tsx
@@ -1,5 +1,56 @@
+import { SearchInput, Title } from '@/components/common';
+import Permission from '@/components/Permission';
+import ICombinedData from '@/types/combinedData';
+import useCombinedData from '@/utils/useCombinedData';
+import useFilteredPermission from '@/utils/useFilteredPermission';
+import styled from '@emotion/styled';
+import { useEffect, useState } from 'react';
+
 const PermissionPage = () => {
-	return <div>Permission Page</div>;
+	const { data } = useCombinedData();
+	const [countries, setCountries] = useState<ICombinedData[]>([]);
+	const filtered = useFilteredPermission(data);
+
+	useEffect(() => {
+		if (!filtered) return;
+
+		setCountries(filtered);
+	}, [filtered]);
+
+	const onSearch = (query: string) => {
+		if (!filtered) return;
+
+		const lowerQuery = query.toLowerCase();
+
+		const filteredData = filtered.filter(
+			(country) =>
+				country.country_nm.toLowerCase().includes(lowerQuery) ||
+				country.country_eng_nm.toLowerCase().includes(lowerQuery),
+		);
+
+		setCountries(filteredData);
+	};
+
+	return (
+		<Container>
+			<Title
+				data={{
+					title: '국가별 입국 허가요건',
+					text: '국가별 입국 가능 기간, 입국 가능 여부, 입국시 여권 소지 여부를 확인할 수 있습니다.',
+				}}
+			/>
+			<SearchInput onSearch={onSearch} placeholder={'국가명을 입력하세요.'} />
+			<Permission data={countries} />
+		</Container>
+	);
 };
 
 export default PermissionPage;
+
+const Container = styled.div`
+	width: 100%;
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	min-height: 0;
+`;
