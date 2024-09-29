5f8dec0a08c58e3a35d68bd329724e38728d40b1
Kim-HyounSuk <kkoor586@naver.com>
2024-09-30 00:18:44 +0900

    Feat: Box 디자인 컴포넌트 수정

--- src/components/common/Box.ts
+++ src/components/common/Box.ts
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 const Box = styled.div`
 	width: 100%;
 	background: ${({ theme }) => theme.gradients.boxGradient};
-	border: 2px solid;
+	border: 1px solid;
 	border-image: ${({ theme }) => theme.gradients.strokeGradient} 1;
 	padding: 1rem;
 `;
