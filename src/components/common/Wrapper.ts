975089546fb4c5e6fa7da6ee7b97af38451d31db
Kim-HyounSuk <kkoor586@naver.com>
2024-09-30 11:31:47 +0900

    Feat: page컴포넌트의 공통 Wrapper 레이아웃 작성

---
+++ src/components/common/Wrapper.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+	width: 100%;
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	min-height: 0;
+`;
+
+export default Wrapper;
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,3 +3,4 @@ export { default as Box } from './Box';
 export { default as Title } from './Title';
 export { default as SearchInput } from './SearchInput';
 export { default as BadgeText } from './BadgeText';
+export { default as Wrapper } from './Wrapper';
