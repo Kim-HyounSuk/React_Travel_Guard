bbc6d0268b8f30ab8b53a1c95aea514d75eac610
Kim-HyounSuk <kkoor586@naver.com>
2024-10-04 15:41:42 +0900

    -: 사용하지 않는 import 제거

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 import { Loading } from '@/components/common';
 import Layout from '@/components/layouts';
 import {
